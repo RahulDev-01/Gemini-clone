8759d993337e74e3504c2b7f649619631a3b09fa
S.Rahul <s.rahul5116@outlook.com>
2025-12-14 10:43:39 +0530

    New Componnets Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useContext, useEffect } from 'react'
+import { Context } from './context/Context'
 
 import './App.css'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
 
 function App() {
+    const { theme } = useContext(Context);
 
+    useEffect(() => {
+        if (theme === 'light') {
+            document.body.classList.add('light-mode');
+        } else {
+            document.body.classList.remove('light-mode');
+        }
+    }, [theme]);
 
- return(
-  <>
-  <Sidebar />
-  <Main />
- </>
- )
+    return (
+        <>
+            <Sidebar />
+            <Main />
+        </>
+    )
 }
 
 export default App
